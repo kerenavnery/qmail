2587cd35a658341db93a0a302e5989e837579ee9
Cica Gustiani <cicagustiani@gmail.com>
2019-09-13 15:49:45 +0100

    first commit

---
+++ Protocols.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from qiskit import *
+from qiskit.quantum_info import Statevector
+
+
+def send_a_qmail(message):
+    """ Alice sends to Bob a quantum email
+
+    :message:int,  
+    """
+
+    Q = 
+    
+
+
+
+
+
+def qotp(qmessage, key):
+    """
+    Quantum one-time pad
+
+    :qmessage:qiksit.QuantumCircuit 
+    :key: dict{x:int y:int} 
+    """
+    #Alice's part
+    Q =1 
+
+    #Bob's part 
+
+
+    
+
