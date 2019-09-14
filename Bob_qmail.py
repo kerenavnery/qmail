bc181b28e20f1d84c5efb775e0b96e932ebd9a52
Cica Gustiani <cicagustiani@gmail.com>
2019-09-14 01:01:16 +0100

    cleanup

--- Bob_qmail.py
+++
@@ -1,9 +0,0 @@
-import socket
-from SocketChannel import SocketChannel
-import Protocols as Prt
-
-channel = SocketChannel(port=5005, listen=True)
-
-data = channel.receive()
-print("received data:", data.decode("utf-8"))
-channel.close()
